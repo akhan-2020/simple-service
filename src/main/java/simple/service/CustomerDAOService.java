da4673de2845ee80f15da6d9390268e8ac2aee12
akhan <akhan@moneygram.com>
2020-03-26 15:47:17 -0500

    initial commit

--- src/main/java/simple/service/CustomerDAOService.java
+++ src/main/java/simple/service/CustomerDAOService.java
@@ -1,14 +1,19 @@
 package simple.service;
 
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.retry.annotation.Retryable;
 import io.reactivex.Single;
 
-@Retryable(delay = "5s")
+//@Retryable(delay = "5s")
 @Client("http://localhost:8181")
 public interface CustomerDAOService {
 
     @Post("/customer")
     Single<Person> save(String firstName, String lastName, String phone);
+
+    @Get("/customer/phone/{phone}")
+    Single<Person> getByPhone(@PathVariable String phone);
 }
--- src/main/java/simple/service/SimpleController.java
+++ src/main/java/simple/service/SimpleController.java
@@ -2,6 +2,7 @@ package simple.service;
 
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.*;
 import io.micronaut.http.client.HttpClient;
@@ -11,6 +12,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import javax.validation.constraints.NotBlank;
+import java.util.HashMap;
+import java.util.Map;
 
 
 //@Controller("/customer/{delay}")
@@ -31,25 +35,43 @@ public class SimpleController {
     private static final Logger logger
             = LoggerFactory.getLogger(SimpleController.class);
 
-    @Get("/{delay}")
-    public String index(@PathVariable String delay) {
 
-        System.out.println("Calling httpbin org api using delay of " + delay);
+    @Get("/delay/{delay}")
+    public String delay(@PathVariable String delay) {
+        logger.info("Calling httpbin org api using delay of " + delay);
         String response = client.toBlocking().retrieve(HttpRequest.GET("/delay/" + delay));
-
         logger.info("response from httpbin delay {} ", response);
-        return "Hello World";
+        return "Hello World from httpbin " + delay;
     }
 
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Post
     public Single<Person> save(@Body Person person) {
+        logger.info("writing customer to MongoDB " + person.getPhone());
         System.out.println("inserting customer " + person.getFirstName());
-
-       // Person response = client.toBlocking().retrieve(HttpRequest.POST("/customer", person));
         return daoSvc.save(person.getFirstName(), person.getLastName(), person.getPhone());
     }
+
+    @Produces(MediaType.APPLICATION_JSON)
+    @Get("/phone/{phone}")
+   // public Single<Person>index(@PathVariable @NotBlank String phone) {
+    public HttpResponse index(@PathVariable @NotBlank String phone) {
+
+        try {
+            logger.info("Searching customer by phone " + phone);
+            Single<Person> p = daoSvc.getByPhone(phone);
+            logger.info("Finsihed searching customer by phone " + phone);
+            return HttpResponse.ok(p.blockingGet());
+        } catch (Exception e) {
+            logger.error("Error searching customer by phone " + e.getMessage());
+            Map<String, Object> m = new HashMap<>();
+            m.put("status", 400);
+            m.put("error", "Invalid request data");
+            return HttpResponse.status(HttpStatus.BAD_REQUEST).body(m);
+        }
+    }
+
 
 
 }
